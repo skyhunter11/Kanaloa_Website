db29d0caa15c41c73642371ba18c60845e58f09a
Kane <ori.lowen@gmail.com>
2025-01-12 14:29:02 -1000

    Remove padding from header when scrolled halfway down the hero section

--- js/header-script.js
+++ js/header-script.js
@@ -5,17 +5,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const logo = document.querySelector(".header__logo");
     const firstSection = document.getElementById("first-section");
 
+    const header = document.querySelector("header");
+
     window.addEventListener("scroll", () => {
         const sectionHeight = firstSection.offsetHeight;
         const scrollPosition = window.scrollY;
 
         // Show logo if scrolled halfway down the first section
         if (scrollPosition > sectionHeight / 2) {
-            logo.style.display = "block"; // Show the logo
+
+            // Show the logo
+            logo.style.display = "block";
             logo.setAttribute("aria-hidden", "false");
+
+            // Remove p-3 class
+            header.classList.remove("p-3")
         } else {
-            logo.style.display = "none"; // Hide the logo
+
+            // Hide the logo
+            logo.style.display = "none";
             logo.setAttribute("aria-hidden", "true");
+
+            // Restore p-3 class
+            header.classList.add("p-3");
         }
     });
 });
